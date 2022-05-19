5dfabd86acffc538810d9c15e03804ca14501c4a
s3818311 <s3818311@rmit.edu.vn>
2022-05-20 02:17:21 +0700

    elevation chart

--- dart-project/src/components/statistic/BarChartContainer.js
+++ dart-project/src/components/statistic/BarChartContainer.js
@@ -3,7 +3,7 @@ import Paper from "@mui/material/Paper";
 import { ResponsiveBar } from "@nivo/bar";
 import Typography from "@mui/material/Typography";
 
-const barData = require("../../tempdata/bar.json");
+const barData = require("../../tempdata/elevation.json");
 
 const BarTitle = () => {
   return (
@@ -13,7 +13,7 @@ const BarTitle = () => {
       align="center"
       sx={{ marginTop: 1, marginBottom: -5 }}
     >
-      Bar Chart Title
+      Ho Chi Minh City elevation from sea level (m)
     </Typography>
   );
 };
@@ -21,13 +21,15 @@ const BarTitle = () => {
 const StatisticBarChart = () => (
   <ResponsiveBar
     data={barData}
-    keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-    indexBy="country"
+    layout="horizontal"
+    keys={["val"]}
+    indexBy="ele"
     margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
     padding={0.3}
     valueScale={{ type: "linear" }}
     indexScale={{ type: "band", round: true }}
     colors={{ scheme: "nivo" }}
+    colorBy="indexValue"
     defs={[
       {
         id: "dots",
@@ -80,7 +82,7 @@ const StatisticBarChart = () => (
       tickSize: 5,
       tickPadding: 5,
       tickRotation: 0,
-      legend: "food",
+      legend: "Height from sea level",
       legendPosition: "middle",
       legendOffset: -40,
     }}
@@ -90,30 +92,6 @@ const StatisticBarChart = () => (
       from: "color",
       modifiers: [["darker", 1.6]],
     }}
-    legends={[
-      {
-        dataFrom: "keys",
-        anchor: "bottom-right",
-        direction: "column",
-        justify: false,
-        translateX: 120,
-        translateY: 0,
-        itemsSpacing: 2,
-        itemWidth: 100,
-        itemHeight: 20,
-        itemDirection: "left-to-right",
-        itemOpacity: 0.85,
-        symbolSize: 20,
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemOpacity: 1,
-            },
-          },
-        ],
-      },
-    ]}
     role="application"
     ariaLabel="Nivo bar chart demo"
     barAriaLabel={function (e) {
@@ -128,7 +106,7 @@ const BarChartContainer = (props) => {
       <Paper
         sx={{
           p: 2,
-          height: 600,
+          height: 1000,
         }}
       >
         <BarTitle />
