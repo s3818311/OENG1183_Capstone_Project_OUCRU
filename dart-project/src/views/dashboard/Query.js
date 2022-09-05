43e46ceeea6fe7603ce9aee834ee64ef8625b417
s3818311 <s3818311@rmit.edu.vn>
2022-09-05 22:32:54 +0700

    update ward selector

--- dart-project/src/views/dashboard/Query.js
+++ dart-project/src/views/dashboard/Query.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Checkbox from "@mui/material/Checkbox";
+import ListItemText from "@mui/material/ListItemText";
+import FormControl from "@mui/material/FormControl";
+
 import tableEmpty from "../../tempdata/query_builder_table_empty";
 import districtsWards from "../../tempdata/districts_wards";
 import wardIds from "../../tempdata/wards_id3";
@@ -19,28 +26,113 @@ const WardSelect = (props) => {
 
   return (
     <div>
-      {districts.map((d) => {
-        return (
-          <div>
-            <b>{d}:</b>{" "}
-            {districtsWards[d].map((ward) => {
-              let k = Object.keys(ward);
-              return (
-                <>
-                  <input
-                    type="checkbox"
-                    id={k[0]}
-                    name="wards"
-                    onChange={handleChange}
-                  ></input>
-                  <label for={k[0]}> {ward[k[0]]} </label>
-                </>
-              );
-            })}
-          </div>
-        );
+      {districts.map((d, d_idx) => {
+        if (props.selectedDistricts.indexOf(d) != -1)
+          return (
+            <div key={d_idx}>
+              <b>{d}:</b>{" "}
+              {districtsWards[d].map((ward, w_idx) => {
+                let k = Object.keys(ward);
+                return (
+                  <div key={w_idx} style={{ display: "inline-block" }}>
+                    <input
+                      type="checkbox"
+                      id={k[0]}
+                      name="wards"
+                      onChange={handleChange}
+                    ></input>
+                    <label htmlFor={k[0]}> {ward[k[0]]} </label>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        else return <div key={d_idx}></div>;
       })}
     </div>
+  );
+};
+
+const MultiSelect = (props) => {
+  const districts = Object.keys(districtsWards);
+
+  const [multiSelected, setMultiSelected] = useState([]);
+  let isAllSelected =
+    districts.length > 0 && multiSelected.length === districts.length;
+
+  const multiselectChange = (e) => {
+    const value = e.target.value;
+    if (value[value.length - 1] === "all") {
+      setMultiSelected(
+        multiSelected.length === districts.length ? [] : districts
+      );
+      props.selectedDistrictsSetter(
+        props.selectedDistricts.length === districts.length ? [] : districts
+      );
+      return;
+    }
+    setMultiSelected(value);
+    props.selectedDistrictsSetter(value);
+  };
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const menuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+    getContentAnchorEl: null,
+    anchorOrigin: {
+      vertical: "bottom",
+      horizontal: "center",
+    },
+    transformOrigin: {
+      vertical: "top",
+      horizontal: "center",
+    },
+    variant: "menu",
+  };
+
+  return (
+    <FormControl
+      sx={{
+        width: "50vw",
+        display: "flex",
+      }}
+    >
+      <Select
+        labelId="multiselect-label"
+        multiple
+        value={multiSelected}
+        onChange={multiselectChange}
+        renderValue={(multiSelected) => multiSelected.join(", ")}
+        MenuProps={menuProps}
+      >
+        <MenuItem value="all">
+          <ListItemIcon>
+            <Checkbox
+              checked={isAllSelected}
+              indeterminate={
+                multiSelected.length > 0 &&
+                multiSelected.length < districts.length
+              }
+            />
+          </ListItemIcon>
+          <ListItemText primary="Select All" />
+        </MenuItem>
+        {districts.map((d, d_idx) => (
+          <MenuItem key={d_idx} value={d}>
+            <ListItemIcon>
+              <Checkbox checked={multiSelected.indexOf(d) > -1} />
+            </ListItemIcon>
+            <ListItemText primary={d} />
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   );
 };
 
@@ -54,6 +146,7 @@ const QueryForm = (props) => {
   const [startDate, setStartDate] = useState(avaiDateRange[datasource][0]);
   const [endDate, setEndDate] = useState(avaiDateRange[datasource][1]);
   const [sortorder, setSortOrder] = useState("ascending");
+  const [selectedDistricts, setSelectedDistricts] = useState([]);
   const [wards, setWards] = useState([]);
 
   const handleSubmit = async (e) => {
@@ -83,7 +176,15 @@ const QueryForm = (props) => {
 
   return (
     <form onSubmit={handleSubmit} id="query-form">
-      <WardSelect wards={wards} wardSetter={setWards} />
+      <MultiSelect
+        selectedDistricts={selectedDistricts}
+        selectedDistrictsSetter={setSelectedDistricts}
+      />
+      <WardSelect
+        wards={wards}
+        wardSetter={setWards}
+        selectedDistricts={selectedDistricts}
+      />
       <label htmlFor="datasource_id">
         Data source:{" "}
         <select
@@ -92,10 +193,9 @@ const QueryForm = (props) => {
           onChange={(e) => {
             setDataSource(e.target.value);
           }}
+          defaultValue="chirps"
         >
-          <option value="chirps" selected>
-            CHIRPS - Rainfall Estimates
-          </option>
+          <option value="chirps">CHIRPS - Rainfall Estimates</option>
           <option value="aphrodite_temperature">
             APHRODITE - Daily Mean Temperature
           </option>
@@ -137,10 +237,9 @@ const QueryForm = (props) => {
           onChange={(e) => {
             setSortOrder(e.target.value);
           }}
+          defaultValue="ascending"
         >
-          <option value="ascending" selected>
-            Ascending
-          </option>
+          <option value="ascending">Ascending</option>
           <option value="descending">Descending</option>
         </select>
       </label>
@@ -158,20 +257,24 @@ const ResponseTable = (props) => {
 
   return (
     <table>
-      <tr>
-        {headers.map((h) => {
-          return <th>{h !== "date" ? wardIds[h] : "date"}</th>;
+      <thead>
+        <tr>
+          {headers.map((h, h_idx) => {
+            return <th key={h_idx}>{h !== "date" ? wardIds[h] : "date"}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {queryData.map((row, r_idx) => {
+          return (
+            <tr key={r_idx}>
+              {Object.values(row).map((value, v_idx) => {
+                return <td key={v_idx}>{value}</td>;
+              })}
+            </tr>
+          );
         })}
-      </tr>
-      {queryData.map((row) => {
-        return (
-          <tr>
-            {Object.values(row).map((value) => {
-              return <td>{value}</td>;
-            })}
-          </tr>
-        );
-      })}
+      </tbody>
     </table>
   );
 };
