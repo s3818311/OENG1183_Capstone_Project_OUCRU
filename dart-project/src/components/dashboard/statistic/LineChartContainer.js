d04c99f537333f9a890c3644ee3952c881d09444
s3818311 <s3818311@rmit.edu.vn>
2022-09-07 17:23:46 +0700

    ready for deploy

--- dart-project/src/components/dashboard/statistic/LineChartContainer.js
+++ dart-project/src/components/dashboard/statistic/LineChartContainer.js
@@ -116,7 +116,7 @@ const LineChartContainer = (props) => {
   useEffect(() => {
     async function fetchChirpsData() {
       setBusy(true);
-      const res = await fetch("http://localhost:9000/chirps");
+      const res = await fetch("http://3.80.95.119:9000/chirps");
       const json = await res.json();
 
       setChirpsData(json);
